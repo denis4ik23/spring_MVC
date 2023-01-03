f3d92c997f8d0a662b4bfdfee7678d2c245465d6
denis4ik23 <denis4ik2387@gmail.com>
2023-01-03 23:36:43 +0300

    Hello mvc java

---
+++ src/main/java/com/denis4ik23/springmvc/config/MySpringDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package com.denis4ik23.springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
