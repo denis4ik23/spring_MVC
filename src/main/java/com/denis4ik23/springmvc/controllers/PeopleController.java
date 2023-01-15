eb64b0efebcadac26a5b195eb3fbc3fa89a9e4ee
denis4ik23 <denis4ik2387@gmail.com>
2023-01-15 20:59:55 +0300

    post

--- src/main/java/com/denis4ik23/springmvc/controllers/PeopleController.java
+++ src/main/java/com/denis4ik23/springmvc/controllers/PeopleController.java
@@ -1,12 +1,10 @@
 package com.denis4ik23.springmvc.controllers;
 
 import com.denis4ik23.springmvc.dao.PersonDAO;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.denis4ik23.springmvc.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +26,17 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){    //(Model model) {
+        //model.addAttribute("person", new Person()); заменили на @ModelAttribute
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
