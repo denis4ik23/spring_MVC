983faa506d6e6180919fc06a62c11d373f28816a
denis4ik23 <denis4ik2387@gmail.com>
2023-01-15 19:58:49 +0300

    merge

--- src/main/java/com/denis4ik23/springmvc/controllers/PeopleController.java
+++ src/main/java/com/denis4ik23/springmvc/controllers/PeopleController.java
@@ -1,7 +1,6 @@
 package com.denis4ik23.springmvc.controllers;
 
 import com.denis4ik23.springmvc.dao.PersonDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
